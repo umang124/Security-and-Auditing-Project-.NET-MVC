081ab69c424f498345af5b002defa08c1d05494d
umangstha124 <umangstha124@gmail.com>
2024-03-20 13:02:21 +0545

    initial commit

---
+++ Security and Auditing Project .NET MVC/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using Security_and_Auditing_Project_.NET_MVC.Models;
+using System.Diagnostics;
+
+namespace Security_and_Auditing_Project_.NET_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Security and Auditing Project .NET MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Security_and_Auditing_Project_.NET_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
